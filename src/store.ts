2bb2fe20ebc4ea289e92f469fcf8beb3fb176a84
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2023-10-26 11:42:09 +0530

    added delete task function in store

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,18 @@
 import { create } from "zustand";
 
+type Task = {
+    title: string,
+    state: string
+}
+
 const store = (set: any) => ({
     tasks: [{title: 'TestTask', state: 'ONGOING'}],
-    addTask: (title: string, state: string) => set((store: any) => ({tasks: [...store.tasks, {title, state}]}))
+    addTask: (title: string, state: string) => set((store: any) => ({tasks: [...store.tasks, {title, state}]})),
+    deleteTask: (title: string) => set((store: any) => (
+        {
+            tasks: store.tasks.filter((task: Task) => task.title !== title)
+        }
+    ))  
 });
 
 export const useStore = create(store);
