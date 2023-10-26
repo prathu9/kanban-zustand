3df6ab628bd279f2ce4c5dfb2cd86c0675f13c1c
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2023-10-26 16:47:01 +0530

    added label to store set functions

--- src/store.ts
+++ src/store.ts
@@ -9,18 +9,18 @@ type Task = {
 const store = (set: any) => ({
     tasks: [{title: 'TestTask', state: 'ONGOING'}],
     draggedTask: null,
-    addTask: (title: string, state: string) => set((store: any) => ({tasks: [...store.tasks, {title, state}]})),
+    addTask: (title: string, state: string) => set((store: any) => ({tasks: [...store.tasks, {title, state}]}), false, "addTask"),
     deleteTask: (title: string) => set((store: any) => (
         {
             tasks: store.tasks.filter((task: Task) => task.title !== title)
         }
-    )),
-    setDraggedTask: (title: string|null) => set({draggedTask: title}),
+    ), false, "deleteTask"),
+    setDraggedTask: (title: string|null) => set({draggedTask: title}, false, "setDraggedTask"),
     moveTask: (title: string, state: string) => set((store: any) => ({
         tasks: store.tasks.map((task: Task) => task.title === title? {
             title, state
         }: task)
-    }))
+    }), false, "moveTask")
 });
 
 export const useStore = create(devtools(store));
