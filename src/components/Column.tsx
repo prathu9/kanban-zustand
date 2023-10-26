db7c6b74b9dc5f9b6291b089e17824e822cbf212
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2023-10-26 11:34:45 +0530

    added add task form and function

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,31 +1,57 @@
-import Task from "./Task"
-import {useMemo} from "react"
-import "./Column.css"
-import { useStore } from "../store"
+import Task from "./Task";
+import { useMemo, useState } from "react";
+import "./Column.css";
+import { useStore } from "../store";
 
 type ColumnProps = {
-    state: string
-}
+  state: string;
+};
 
-const Column = ({state}: ColumnProps) => {
-    const tasks = useStore(store => store.tasks)
+const Column = ({ state }: ColumnProps) => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-    // React way to filter without unecessary rerendering
-    const filteredTasks = useMemo(() => tasks.filter(task => task.state === state), [tasks, state])
+  const tasks = useStore((store) => store.tasks);
 
-    const addTask = useStore(store => store.addTask);
-console.log(state)
-    return(
-        <div className="column">
-            <div className="titleWrapper">
-                <p>{state}</p>
-                <button onClick={() => {addTask('asdsa'+state, state)}}>Add</button>
-            </div>
-            {filteredTasks.map((task) => (
-                <Task title={task.title} key={task.title}/>
-            ))}
+  // React way to filter without unecessary rerendering
+  const filteredTasks = useMemo(
+    () => tasks.filter((task) => task.state === state),
+    [tasks, state]
+  );
+
+  const addTask = useStore((store) => store.addTask);
+
+  return (
+    <div className="column">
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
+      {filteredTasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+      {open ? (
+        <div className="modal">
+          <div className="modalContent">
+            <input
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
         </div>
-    )
-}
+      ) : null}
+    </div>
+  );
+};
 
 export default Column;
